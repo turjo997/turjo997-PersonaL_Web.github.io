e7d1101c0bdeec11fa9a2301932536230850b748
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-02-18 21:35:29 +0600

    Add files via upload

---
+++ personal-website/index.php
@@ -0,0 +1,421 @@
+
+
+<!DOCTYPE html>
+
+<html>
+     <head>
+	 
+	        <title>Personal-Website</title>
+            <meta charset = "UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+			<link rel = "stylesheet" href="style.css"/>	
+
+      <!--
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous">	
+        
+
+      -->
+
+      
+
+                <!--font awsome-->
+
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+
+            <!--google font-->
+              <!--        <link rel="preconnect" href="https://fonts.gstatic.com">-->
+    
+            <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:wght@100&family=Roboto:wght@100&display=swap" rel="stylesheet">
+		
+               <!---bootstrapt cdn--->
+		        	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
+		<style>
+
+            
+
+             
+        </style>
+
+	 </head>
+	 
+	 <body>
+           
+           <div class="container-fluid">
+
+            <header id ="Home">
+
+                <nav class="navbar navbar-expand-md">
+                   
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                      <span><i class="fas fa-bars navbar-icon"></i></span>
+                    </button>
+
+                    <a class="navbar-brand" href="#">
+                          <img src="https://st2.depositphotos.com/3867453/10635/v/600/depositphotos_106357766-stock-illustration-letter-t-logo-icon-design.jpg" alt="logo">
+                    </a>
+
+
+                    <div class="collapse navbar-collapse"  id="navbarNavDropdown">
+                      <ul class="navbar-nav m-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link text-center text-md-left" href="#Home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link text-center text-md-left" href="#about-me">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link text-center text-md-left" href="#features">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-center text-md-left" href="#feedback">Personal Likings</a>
+                          </li>
+                    
+                          <li class="nav-item">
+                            <a class="nav-link text-center text-md-left" href="#contact">Contact</a>
+                          </li>
+                      
+                    
+                      </ul>
+                    </div>
+                  </nav>
+
+
+
+                  <div class="section">
+
+                    <div class="banner-container d-flex justify-content-center align-items-center">
+
+                         <div class="banner-contents text-center">
+
+                            <h1 class="font-weight-bolder text-warning mb-5 ">Want to know about me !!!</h1>
+
+                            <!---
+                             <button class="btn btn-lg btn-success mr-3" onclick="location.href=https://www.facebook.com/profile.php?id=100009652562164"><i class="fab fa-facebook"></i> Visit facebook</button>
+                             <button class="btn btn-lg btn-success mr-3"><i class="fab fa-instagram"></i>Visit Instagram</button>
+                            -->
+                             <a class = "btn btn-lg btn-success mr-3" href="https://www.facebook.com/profile.php?id=100009652562164"><i class="fab fa-facebook"></i>Visit Facebook</a>
+                             
+                             <a class = "btn btn-lg btn-success mr-3" href="https://www.instagram.com/ullash_bhattacharjee/"><i class="fab fa-instagram"></i>Visit Instagram</a>
+
+                         </div>
+
+
+                    </div>
+
+                  </div>
+
+
+            </header>
+
+            <main>
+
+                
+                    <!--About Me-->
+
+                    <div class="about-section" id="about-me">
+
+                        <div class="jumbotron">
+
+                         
+                        <div class="title text-center mb-3" id = "Bold_about_me">
+                         <h3 class="font-weight-bolder">About Me</h3>
+                       </div>
+                          <div class="row">
+                             <div class="col-lg-4 text-center">
+                                 <img class = " d-block m-auto rounded-circle img-fluid"src="https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p240x240/95659004_1198712097127198_8136623973845696512_n.jpg?_nc_cat=101&ccb=2&_nc_sid=7206a8&_nc_eui2=AeE5EMZZn4-KKOThMk1Ta3DjzT-87aiTc1zNP7ztqJNzXHvuJG92OSzDIXFXZk7N_jt5aztLL1A9PxyA42PF5pII&_nc_ohc=_1a7g-lKptMAX9WDcKj&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=73864c22f2c89f3da93e8fc522d6fb75&oe=60342229" alt="">
+
+                             </div>
+
+                             <div class="font-weight-bolder text-success col-lg-8" id="my_sayings">
+                                 <p>Hello!!!. I am Ullash  Bhattacharjee. I have completed my SSC from Willes Little Flower School  And College, and HSC from GOVT.SCIENCE College. Right now, I am pursuing  B.Sc in Computer Science and Engineering  from Ahsanullah University of Science and Technology. I like watching detective movies and spending time with my families. Besides, I also love to solve competitive programming problems. I have research interest in Computer Science field over AI(Artificial Intelligence).</p>
+                                  
+                                  <a class = "d-block mb-5" href="#demo" data-toggle="collapse">Learn More</a>
+
+                                  <div id = demo class="collapse">
+                                        <p>In future I want  to work in this field, although  currently I have not much such  knowledge  about it. I am preparing  myself  to gather knowledge . Hopefully , will contribute something in this field.</p>
+                                      
+                                  </div>
+                                 
+                                <div class="alert alert-success alert-warning">
+                                 
+                                 <button class="close" data-dismiss="alert">&times?</button>
+                                 Have you visited my twitter? <a href="https://twitter.com/UllashBhattach1">Click Here</a>
+
+
+                                </div>
+
+                                <div class="alert alert-success alert-dismissable">
+                                 
+                                  <button class="close" data-dismiss="alert">&times?</button>
+                                  Have you visited my Github? <a href="https://github.com/turjo997">Click Here</a>
+ 
+ 
+                                 </div>
+
+                                
+
+                             </div>
+         
+                          </div>
+
+                          
+                        </div>
+
+
+                    </div>
+
+
+                <section id="features"class="text-center"> 
+                    <div class="row">
+
+                          <div class="col-lg-4 d-block d-lg-flex">
+
+                            <div class="features-col">
+                                <i class="fas fa-hand-holding-usd fa-3x icon-style rounded-circle "></i>
+                                <h5 class="font-weight-bolder">Happy Transactions</h5>
+                                <p class="font-weight-bold">Transaction plays a vital role in our day to day life. A transaction is a completed agreement between a buyer and a seller to exchange goods, services, or financial assets. We should be more careful about transactions while using it.</p>
+
+                            </div>
+
+                          </div>
+                          <div class="col-lg-4 d-block d-lg-flex">
+
+                            <div class="features-col">
+                           <i class="text-white rounded-circle icon-style fa-3x fas fa-hands-helping"></i>
+                          
+                             
+                                <!--<i class="fas fa-hands-helping-usd fa-3x icon-style rounded-circle "></i>-->
+                                <h5 class="font-weight-bolder">Spread Help</h5>
+                                <p class="font-weight-bold">The COVID-19 pandemic makes us all more vulnerable as we worry about the spread of the disease, work interruptions, financial insecurity, upheavals in our daily routines, and our loss of social life.Let’s keep showing our local love for everyone's well-being.</p>
+
+                            </div>
+
+                          </div>
+                          <div class="col-lg-4 d-block d-lg-flex">
+
+                            <div class="features-col">
+
+                              <i class="far fa-smile fa-3x icon-style "></i>
+                                <!--<i class="fas fa-hand-holding-usd fa-3x icon-style rounded-circle "></i>-->
+                                <h5 class="font-weight-bolder">Smile</h5>
+                                <p class="font-weight-bold">Always Smilee!!! Smiling not only offers a mood boost but helps our bodies release cortisol and endorphins that provide numerous health benefits. Smile Reduced blood pressure , Increased endurance , Reduced stress,Strengthened immune system.</p>
+
+                            </div>
+
+                          </div>
+                    </div>
+
+                  </section>
+
+
+                    <!--Feedback-->
+            
+                    <section class="container-fluid text-white" id ="feedback">
+
+                        <div class="section-title">
+                             <h3 class="text-center text-light">Personal Likings</h3>
+                             <hr class="hr-style">
+                        </div>
+
+                        <div class="row">
+
+                           <div class="col-lg-12">
+
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="2000" data-pause="hover">
+                                <ol class="carousel-indicators">
+                                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                      <p>Steve jobs is my favourite Personality.Doesn't need to introduce him again , since he is widely known public figure and outstanding personality. He was the chairman, chief executive officer (CEO), and co-founder of Apple Inc. He was a very hard working person with the most intellectual brain of his entire life. He is my idol and will be.</p>
+                                       <img style ="width:300px ;"class = "feedback-img rounded-circle mb-3"src="https://cdn.vox-cdn.com/thumbor/rES5fxTJl-z014NcJV7Rradtxrc=/0x86:706x557/1400x1400/filters:focal(0x86:706x557):format(png)/cdn.vox-cdn.com/imported_assets/847184/stevejobs.png" alt="">
+                                       <h2 class= "font-weight-bolder text-white">Steve jobs</h2>
+                                     
+                                       
+                                  </div>
+                                  <div class="carousel-item">
+                                    <p>In my personal interest , I also like watching crime thrilling movies and series. Breaking Bad is one of them. This is an American neo-Western crime drama television series created and produced by Vince Gilligan. The gradual development of building up the Walter White character always caught my eye.</p>
+                                    <img style ="width:450px;" class = "feedback-img rounded-square mb-3"src="https://i.ytimg.com/vi/ceqOTZnhgY8/maxresdefault.jpg" alt="">
+                                   
+                                
+                                  </div>
+                                  <div class="carousel-item">
+                                       <p>As I've said earlier , I am a big fan of any kind of detective stuffs. For Novels , it is oviously "FELUDA SOMOGRO". The author of this novel is "Satyajit Ray". He was one of the best detective author in his time being and also now. His immortal creation of building up the character Feluda is just mild-blowing and heart touching.</p>
+                                       <img style="width: 300px;"class = "feedback-img  mb-3"src="https://books.anandodhara.com/wp-content/uploads/2016/06/feluda-samagara-Vol12-by-satyajit-ray-front-cover.jpg" alt="">
+                                       
+                                  </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="sr-only">Next</span>
+                                </a>
+                              </div>
+
+                           </div>
+
+                        </div>
+
+
+
+                    </section>
+
+            <div class="contact-section text-white p-5 bg-success" id="contact">
+
+                <div class="title text-center mb-3">
+                  <h3 class="font-weight-bolder">Contact Me</h3>
+                </div>
+
+
+                <form method = "post" action="" class="w-50 m-auto was-validated">
+
+                  <div class="from-group">
+                     
+                    <label for="fullname">Full Name</label>
+                    <input type="text" class="form-control" name="fullname" required>
+
+                    <div class="valid-feedback">Valid</div>
+                    <div class="invalid-feedback">Please enter your fullname as soon as possible</div>
+
+                  </div>
+
+                  <div class="from-group">
+
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email">
+
+                    
+                  </div>
+
+                  <div class="from-group">
+
+                    <label for="subject">Subject</label>
+                    <input type="text" class="form-control" name="subject">
+
+                    
+                  </div>
+
+                  <div class="from-group">
+
+                    <label for="message">Message</label>
+                    <textarea style = "resize: none;" name="message" class = "form-control" id="" cols="30" rows="6"></textarea>
+
+                    
+                  </div>
+
+                  <div class="form-group mt-3">
+
+                   <input type="submit" name="submit" class= "btn btn-secondary" value="submit">
+                   
+                    <input type="reset"  class= "btn btn-secondary"  value="reset">
+
+                                        
+                    <?php
+                        include_once 'send.php';
+                    ?>
+
+                                  
+                  </div>
+                   
+                </form>
+                
+                
+        
+              </div>
+
+    
+              
+            </main>
+
+
+            <footer>
+
+              <div class="row text-white">
+
+                  <div class="col-lg-6 text-center text-lg-left mb-3 mb-lg-0">
+                          <p class="small mb-0 mt-1">&copy; Copyright Ullash Bhattacharjee</p>
+
+                  </div>
+                  
+                  <div class="col-lg-6 text-center text-lg-right">
+                    <a href="#Home">Home</a>
+                    <a href="https://www.facebook.com/?stype=lo&jlou=AfdrYyBvqmmuYrSgsoPQP6TriwwnkRaxEDSA48E3d-hAvS5R6_QNy7SpgAEUaDI5YBboPCL9msGHc0UPegtYWpMyE--Hr9rt_6RZSjD4gnViaA&smuh=39442&lh=Ac-TL8BLZaSJsNo0G6Q"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://twitter.com/login/error?username_or_email=Turjo&redirect_after_login=%2F"><i class="fab fa-twitter"></i></a>
+                  </div>
+
+              </div>
+
+            </footer>
+
+
+
+
+
+           </div>
+  
+<!---
+          <script type="text/javascript">
+
+          if(window.history.replaceState){
+            window.history.replaceState(null , null , window.location.href);
+          }
+            </script>
+
+
+     -->      
+ <!---
+          
+        <script src="https://unpkg.com/swup@latest/dist/swup.min.js"></script>
+          <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.5/typed.min.js"></script>
+               
+           <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+           -->
+
+
+         
+
+
+		       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+           <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+	 </body>
+
+
+
+</html>
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
---
+++ personal-website/send.php
@@ -0,0 +1,71 @@
+
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+//require 'class.phpmailer.php';
+
+
+//require 'class.smtp.php';
+
+require_once "PHPMailer/PHPMailer.php";
+require_once "PHPMailer/SMTP.php";
+require_once "PHPMailer/Exception.php";
+
+
+$mail=new PHPMailer(true);
+
+
+
+
+$result="";
+  if(isset($_POST['submit']))
+  {
+  
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $name=$_POST['fullname'];
+    $mail->isSMTP();
+    $mail->Debugoutput = 'html';
+    $mail->Host='smtp.gmail.com';
+    $mail->Port= 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure='tls';
+    $mail->Username="turjouser98@gmail.com";
+    $mail->Password='98turjouser98';
+
+    $mail->addAddress('turjouser98@gmail.com');
+    $mail->isHTML(true);
+    $mail->subject=$subject;
+
+
+    $mail->Body = "<h3> <br>Email : $email <br> <h3> <br>Name : $name <br> Subject : $subject <br> Message : $message</h3>";
+
+
+   
+    if(!empty($email))
+    {
+     
+      $mail->setFrom("$email","$name");
+      if($mail->send()){
+      $email=null;
+
+
+      $result = '<div class="alert alert-success">
+                 <h5>Thankyou! for contacting us, We\'ll get back to you soon!</h5>
+               </div>';
+               echo "$result";
+      
+           }
+    }
+    else {
+
+      echo " not send";
+
+    }
+
+
+  }
+
+
+ ?>
